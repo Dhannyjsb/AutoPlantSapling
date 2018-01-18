84e200117e39637db93b830931c93490bb512ff7
BuildTools <unconfigured@null.spigotmc.org>
2018-01-18 10:32:47 +0800

    test

--- src/com/inferniaserver/autoplantsapling/Main.java
+++ src/com/inferniaserver/autoplantsapling/Main.java
@@ -3,21 +3,19 @@ package com.inferniaserver.autoplantsapling;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.BlockState;
-import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
 	
 	 public Block block;
 	 public Material materi;
-	 private byte data;
+
 	 
 	 @Override
 	 public void onEnable() {
 		// this.saveDefaultConfig();
 		 this.materi = Material.SAPLING;
-		 this.data = -1;
+
 	 }
 	 
 	 public void onDisable() {
@@ -28,12 +26,7 @@ public class Main extends JavaPlugin{
 		 Material below = this.block.getRelative(BlockFace.DOWN).getType();
 		 	if  (below == Material.DIRT || below == Material.GRASS){
 		 		 this.block.setType(materi);
-		            if (data < 0) {
-		            	BlockState state = block.getState();
-		            	MaterialData data = state.getData();
-		            	state.setData(data);
-		            	state.update();
-		            }
+
 		 	}
 	 }
 	 
